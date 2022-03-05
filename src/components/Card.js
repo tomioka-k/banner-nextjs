421aca481f8c7f931d2ff7a8c897d99b69ffd52e
tomioka-k <tomioka-k@tajima.jp>
2022-03-06 02:14:22 +0900

    modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,20 @@
 import Image from "next/image";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 export default function Card({ image }) {
   return (
     <section className="px-2 py-2">
       <figure className="bg-gray-50 rounded-3xl">
-        <Image
-          className="rounded-2xl justify-items-center"
-          src={image}
-          width={350}
-          height={350}
-          objectFit="contain"
-        />
+        <Zoom>
+          <Image
+            className="rounded-2xl justify-items-center"
+            src={image}
+            width={350}
+            height={350}
+            objectFit="contain"
+          />
+        </Zoom>
       </figure>
     </section>
   );
