3400fef8ccf7d53482dd585db54bc437bd86b500
tomioka-k <tomioka-k@tajima.jp>
2022-03-08 02:10:55 +0900

    card design edit

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export default function Card({ image }) {
   return (
     <section className="px-2 py-2">
       <figure className="bg-white shadow rounded-md px-2.5 pt-2.5 pb-1">
-        <Zoom zoomMargin={50}>
+        <Zoom zoomMargin={50} overlayBgColorEnd={" #EFF6FF"}>
           <Image src={image} width={1000} height={1000} objectFit="contain" />
         </Zoom>
       </figure>
